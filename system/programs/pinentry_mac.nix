eb39de3ac74b07e1d54e321c7c8c04271141a99c
MrZeLee <mrzelee404@gmail.com>
2025-03-03 11:55:42 +0000

    feat(pinentry_mac.nix): create a symlink for pinentry-mac to make 'pinentry' available

--- system/programs/pinentry_mac.nix
+++ system/programs/pinentry_mac.nix
@@ -2,15 +2,24 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  myPinentryMac = pkgs.pinentry_mac.overrideAttrs (old: {
+    installPhase =
+      (old.installPhase or "")
+      + ''
+        # Create a symlink so that 'pinentry' is available
+        ln -s $out/bin/pinentry-mac $out/bin/pinentry
+      '';
+  });
+in {
   environment = {
-    systemPackages = with pkgs;
+    systemPackages =
       []
       ++ lib.optionals pkgs.stdenv.isDarwin [
-        pinentry_mac
+        myPinentryMac
       ];
     variables = lib.mkIf pkgs.stdenv.isDarwin {
-        PINENTRY_PROGRAM = "${pkgs.pinentry_mac}/bin/pinentry-mac";
-      };
+      PINENTRY_PROGRAM = "${pkgs.pinentry_mac}/bin/pinentry-mac";
+    };
   };
 }
