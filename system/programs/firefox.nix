53053a6c5df6f738091b28fa5946b474d0fde146
MrZeLee <mrzelee404@gmail.com>
2025-04-30 00:07:13 +0100

    feat(firefox.nix): use librewolf package and add policies for extensions

--- system/programs/firefox.nix
+++ system/programs/firefox.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
+    package = pkgs.librewolf;
     preferencesStatus = "default";
     preferences = {
       "media.ffmpeg.vaapi.enabled" = true;
@@ -17,6 +18,41 @@
       "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
       "network.trr.mode" = 3;
       "browser.sessionstore.restore_on_demand" = false;
+    };
+    policies = {
+      ExtensionSettings = {
+        "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        "vimium-c@gdh1995.cn" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        "keepassxc-browser@keepassxc.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/keepassxc-browser/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        "newtab-adapter@gdh1995.cn" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/newtab-adapter/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+        "addon@darkreader.org" = {
+          install_url =
+            "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "normal_installed";
+          private_browsing = true;
+        };
+      };
     };
   };
 }
