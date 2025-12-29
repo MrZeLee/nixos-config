770b288c02b2086004f5a6f25dabaeb7c8ed97a7
MrZeLee <mrzelee@proton.me>
2025-12-29 09:24:22 +0000

    fix(cli.nix): update julia and zulu package names to their current versions

--- hosts/home-manager/jmoura/cli.nix
+++ hosts/home-manager/jmoura/cli.nix
@@ -90,7 +90,7 @@
     php83Packages.composer
     lua51Packages.lua
     lua51Packages.luarocks
-    julia_19-bin
+    julia-bin
     python312
     python312Packages.pip
     uv
@@ -98,7 +98,7 @@
     unstable.rustc
     cargo
     nodejs_24
-    zulu23
+    zulu
 
     # Build tools
     gnumake
