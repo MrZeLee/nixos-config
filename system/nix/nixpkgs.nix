5ba92f4d2967374f32c2a87331d128e5451b6796
MrZeLee <mrzelee@proton.me>
2026-01-03 16:54:00 +0000

    feat(nixpkgs.nix): add essential libraries for nix-ld to support Unity games

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -11,8 +11,41 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
+      # Base libraries
       glibc
       libxcrypt-legacy
+
+      # Graphics libraries (for Unity games)
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXext
+      xorg.libXrender
+      xorg.libXfixes
+
+      # OpenGL/Vulkan
+      libGL
+      libglvnd
+      vulkan-loader
+
+      # Audio libraries
+      alsa-lib
+      libpulseaudio
+
+      # Common system libraries
+      zlib
+      stdenv.cc.cc.lib
+
+      # Wayland support
+      wayland
+      libxkbcommon
+
+      # Additional libraries Unity might need
+      dbus
+      fontconfig
+      freetype
     ];
   };
 
